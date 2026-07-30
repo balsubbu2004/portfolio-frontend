d0071aa5fec9aea10b9216550e1b4cbc2b0b39f1
S Bala Subramanyam <balsubbu2004@gmail.com>
2026-07-30 12:21:26 +0530

    Change API base URL to new endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://web-production-057db.up.railway.app/api/',
+  baseURL: 'https://portfolio-backend-owrq.onrender.com/api/',
 });
 
 export default api;
